6651d30723e3a2589dadfcdc6d0acda06cadf30c
david-vargas98 <edgar981125@live.com.mx>
2024-01-08 10:29:14 -0600

    chore: Retorno y nombre de rutas
    Se asigna un nombre a la ruta dashboard, y se retorna la vista chirps en la ruta
    
    Closes N/A

--- routes/web.php
+++ routes/web.php
@@ -9,13 +9,13 @@ Route::get('/', function () {
 
 //Estas rutas se agregan con el paquete de autenticación breeze
 Route::middleware('auth')->group(function () {
-    Route::view('/dashboard', 'dashboard'); //Como solo es una vista, se usa el método view
+    Route::view('/dashboard', 'dashboard')->name('dashboard'); //Como solo es una vista, se usa el método view
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
     //Se agrega también la rita chirps al group, para usar la utenticación
     Route::get('/chirps', function () {
-        return 'ejemplo';
+        return view('chirps.index');
     })->name('chirps.index');
 });
 
