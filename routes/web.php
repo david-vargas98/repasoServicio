e1c160f8d9475d6ff320943db02295bd87eca2ac
david-vargas98 <edgar981125@live.com.mx>
2024-01-08 10:03:20 -0600

    chore: Agrupación de rutas
    Se agrupan todas las rutas en el middleware auth, así como se simplifica la ruta del dashboard, ya que solo regresa una simple vista
    
    Closes N/A

--- routes/web.php
+++ routes/web.php
@@ -7,19 +7,16 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/chirps', function () {
-    return 'ejemplo';
-})->name('chirps.index');
-
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
-
-//Estas rutas se agrgan con el paquete de autenticación breeze
+//Estas rutas se agregan con el paquete de autenticación breeze
 Route::middleware('auth')->group(function () {
+    Route::view('/dashboard', 'dashboard'); //Como solo es una vista, se usa el método view
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    //Se agrega también la rita chirps al group, para usar la utenticación
+    Route::get('/chirps', function () {
+        return 'ejemplo';
+    })->name('chirps.index');
 });
 
 require __DIR__.'/auth.php';
