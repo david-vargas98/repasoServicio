790517162f80410c951e3a4903715da7fc54afcc
david-vargas98 <edgar981125@live.com.mx>
2024-01-09 08:28:53 -0600

    refactor: Reubicación de atributo foráneo
    Se borra el atributo de llave foránea por cuestiones de seguridad del filable, para pasarlo a una relación entre tablas
    
    Closes N/A

--- app/Models/Chirp.php
+++ app/Models/Chirp.php
@@ -8,5 +8,5 @@ use Illuminate\Database\Eloquent\Model;
 class Chirp extends Model
 {
     use HasFactory;
-    protected $fillable = ['message', 'user_id'];
+    protected $fillable = ['message'];
 }
