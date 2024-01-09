c22f27470c51e26177e1cc7d1adfccd5eed329c7
david-vargas98 <edgar981125@live.com.mx>
2024-01-08 19:25:15 -0600

    refactor: Simplificación de rutas
    Se mandó la lógica del los métodos a los controladores y se llamó a sus respectivos métodos
    
    Closes N/A

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,9 @@
 <?php
 
+use App\Http\Controllers\ChirpController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-use App\Models\Chirp;
+
 Route::get('/', function () {
     return view('welcome');
 });
@@ -13,23 +14,11 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-    //Se agrega también la rita chirps al group, para usar la utenticación
-    Route::get('/chirps', function () {
-        return view('chirps.index');
-    })->name('chirps.index');
+    //Se agrega también la ruta chirps al group, para usar la utenticación
+    Route::get('/chirps', [ChirpController::class, 'index'])->name('chirps.index');
 
     //Ruta para el método post del formulario
-    Route::post('/chirps', function(){
-        //Inserción en la BD
-        Chirp::create([
-            'message' => request('message'), //Mensaje que el usuaeio escribió
-            'user_id' => auth()->id(), //Usuario autenticado
-        ]);
-        //Mensaje de sesión
-        
-        //Retorno a la ruta
-        to_route('chirps.index');
-    });
+    Route::post('/chirps', [ChirpController::class, 'store'])->name('chirps.store');
 });
 
 require __DIR__.'/auth.php';
